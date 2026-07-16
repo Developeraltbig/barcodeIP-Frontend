0d54ad6f7a8f8004f6720e3eb2ba89ba0db62719
Roshan <roshan@altbig.com>
2026-07-16 17:31:45 +0530

    updated vite config file

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,13 +1,38 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import jsconfigPaths from "vite-jsconfig-paths";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: "0.0.0.0",
-    port: 3000,
-    strictPort: true,
-    allowedHosts: true,
-  }
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    base: "/",
+
+    plugins: [
+      react(),
+      jsconfigPaths(),
+    ],
+
+    server: {
+      host: "0.0.0.0",
+      port: 3000,
+      strictPort: true,
+      open: true,
+      allowedHosts: true,
+    },
+
+    preview: {
+      host: "0.0.0.0",
+      port: 3000,
+      open: true,
+    },
+
+    define: {
+      global: "window",
+    },
+
+    resolve: {
+      preserveSymlinks: true,
+    },
+  };
+});
