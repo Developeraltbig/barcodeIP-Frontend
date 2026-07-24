ebc8e054b26a88671996a5338ae107c0e51988e9
Roshan <roshan@altbig.com>
2026-07-24 18:28:24 +0530

    remove unnessary

--- src/routes/PagesRoutes.jsx
+++ src/routes/PagesRoutes.jsx
@@ -4,10 +4,6 @@ import { lazy } from 'react';
 import Loadable from 'components/Loadable';
 import MinimalLayout from 'layouts/minimalLayout';
 import LoginN from 'views/auth/work-N/LoginN';
-import HomeN from '../views/auth/work-N/HomeN/HomeN';
-import Footer from '../views/auth/work-N/Footer/Footer';
-import ResultN from '../views/auth/work-N/ResultN/ResultN';
-import Result_2 from '../views/auth/work-N/ResultN/Result_2';
 import Register from 'views/auth/work-N/Register';
 import ForgotPassword from '../views/auth/work-N/ForgotPassword';
 import ResetPassword from '../views/auth/work-N/ResetPassword';
@@ -52,27 +48,6 @@ const PagesRoutes = {
           path: 'reset-password',
           element: <ResetPassword />
         },
-        {
-          path: 'HomeN',
-          element: <HomeN />
-        },
-        {
-          path: 'footer',
-          element: <Footer />
-        },
-        {
-          path: 'result',
-          element: <ResultN />
-        },
-        {
-          path: 'result2',
-          element: <Result_2 />
-        }
-
-        // {
-        //   path: 'register',
-        //   element: <RegisterPage />
-        // }
       ]
     }
   ]
